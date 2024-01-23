726e1e47bd7835468334dbcf7d8f4f24ff34b239
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-01-24 01:11:59 +0530

    committed

--- src/Shopping.jsx
+++ src/Shopping.jsx
@@ -19,9 +19,6 @@ const Shopping = () => {
   const datas=shoppingcart.data;
 
 
-
-
-
   
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
   document.documentElement.classList.add('dark')
